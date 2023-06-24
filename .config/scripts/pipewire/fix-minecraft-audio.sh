77f4f3b260b261e24053c593cdb002a9116125dd
Mrugacz <mrugalski.kuba@gmail.com>
2023-06-25 00:40:09 +0200

    added minecraft audio script to autostart because it does not work in the launcher - i'll try to fix that later

--- .config/scripts/pipewire/fix-minecraft-audio.sh
+++ .config/scripts/pipewire/fix-minecraft-audio.sh
@@ -2,9 +2,16 @@
 # for some unknown reason MC detects a random, disabled audio device as "system default"
 # this fixes it
 
-# just add it as a wrapper command or sth in the launcher
+while true; do
+    if pw-link -o | grep alsoft > /dev/null; then 
+        echo found
+        # left channel
+        pw-link alsoft:channel_1 bluez_output.90_7A_58_D5_E2_7B.1:playback_FL
+        # right channel
+        pw-link alsoft:channel_2 bluez_output.90_7A_58_D5_E2_7B.1:playback_FR
+    else
+        echo waiting
+        sleep 2
+    fi
+done
 
-# left channel
-pw-link alsoft:channel_1 bluez_output.90_7A_58_D5_E2_7B.1:playback_FL
-# right channel
-pw-link alsoft:channel_2 bluez_output.90_7A_58_D5_E2_7B.1:playback_FR
