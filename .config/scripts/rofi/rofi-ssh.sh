e82cd5b5addae2f8bdb7ada42ea52481c18ba619
mrugacz <mrugalski.kuba@gmail.com>
2023-10-19 19:44:41 +0200

    updated printer url

--- .config/scripts/rofi/rofi-ssh.sh
+++ .config/scripts/rofi/rofi-ssh.sh
@@ -2,13 +2,13 @@
 # should work with dmenu
 # fix gnome keyring
 export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/keyring/ssh"
-SERVER=$(echo -e "mrugacz.xyz\nc1872.lh.pl\nender.mrugacz.xyz\nvps.mrugacz.xyz" | rofi -dmenu)
+SERVER=$(echo -e "mrugacz.xyz\nc1872.lh.pl\nvoron.mrugacz.xyz\nvps.mrugacz.xyz" | rofi -dmenu)
 
 TERMCMD='kitty -e ssh'
 
 case $SERVER in 
 	mrugacz.xyz) $TERMCMD mrugacz.xyz ;;
 	c1872.lh.pl) $TERMCMD c1872.lh.pl ;;
-	ender.mrugacz.xyz) $TERMCMD ender.mrugacz.xyz ;;
+	voron.mrugacz.xyz) $TERMCMD voron.mrugacz.xyz ;;
 	vps.mrugacz.xyz) $TERMCMD vps.mrugacz.xyz
 esac
