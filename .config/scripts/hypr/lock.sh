bf5505864d6dba01edbd017644d4f3943510a19a
mrugacz <mrugalski.kuba@gmail.com>
2023-10-19 20:27:53 +0200

    updated colors

--- .config/scripts/hypr/lock.sh
+++ .config/scripts/hypr/lock.sh
@@ -6,30 +6,30 @@ swaylock \
     --screenshots --clock --indicator-idle-visible \
 	--indicator-radius 100 \
 	--indicator-thickness 10 \
-	--ring-color 1a1b26 \
-	--key-hl-color c0caf6 \
-    --bs-hl-color 283457 \
-    --caps-lock-bs-hl-color c0caf5 \
-    --caps-lock-key-hl-color e0af68 \
-	--text-color c0caf5 \
+	--ring-color 665c54 \
+	--key-hl-color ebdbb2 \
+    --bs-hl-color 7c6f64 \
+    --caps-lock-bs-hl-color ebdbb2 \
+    --caps-lock-key-hl-color 7c6f64 \
+	--text-color ebdbb2 \
 	--line-color 00000000 \
-	--inside-color 00000088 \
-	--ring-wrong-color f7768e \
-	--text-wrong-color c0caf5 \
+	--inside-color 1d2021 \
+	--ring-wrong-color cc241d \
+	--text-wrong-color ebdbb2 \
 	--line-wrong-color 00000000 \
-	--inside-wrong-color 00000088 \
-	--ring-ver-color 7aa2f7 \
-	--text-ver-color c0caf5 \
+	--inside-wrong-color 1d2021 \
+	--ring-ver-color 458588 \
+	--text-ver-color ebdbb2 \
 	--line-ver-color 00000000 \
-	--inside-ver-color 00000088 \
-	--ring-clear-color e0af68 \
-	--text-clear-color c0caf5 \
+	--inside-ver-color 1d2021 \
+	--ring-clear-color 7c6f64 \
+	--text-clear-color ebdbb2 \
 	--line-clear-color 00000000 \
-	--inside-clear-color 00000088 \
-	--ring-caps-lock-color e0af68 \
-	--text-caps-lock-color c0caf5 \
+	--inside-clear-color 1d2021 \
+	--ring-caps-lock-color 7c6f64 \
+	--text-caps-lock-color ebdbb2 \
 	--line-caps-lock-color 00000000 \
-	--inside-caps-lock-color 00000088 \
+	--inside-caps-lock-color 1d2021 \
 	--separator-color 00000000 \
 	--fade-in 0.25 \
     --effect-scale 0.5 --effect-blur 7x3 --effect-scale 2 \
