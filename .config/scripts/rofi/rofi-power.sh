19b07c8f30b33d867a5d5f9a2ddb8a8513696d18
mrugacz <mrugalski.kuba@gmail.com>
2024-03-01 19:05:07 +0100

    added reboot to uefi

--- .config/scripts/rofi/rofi-power.sh
+++ .config/scripts/rofi/rofi-power.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-SELECTION=$(echo -e "Log out\nPower off\nReboot" | rofi -dmenu -i)
+SELECTION=$(echo -e "Log out\nPower off\nReboot\nUEFI" | rofi -dmenu -i)
 
 case $SELECTION in
 	'Log out') hyprctl dispatch exit;;
 	'Power off') poweroff;;
 	Reboot) reboot;;
+    UEFI) systemctl reboot --firmware-setup;;
 	*) echo 'no option was selected';;
 esac
