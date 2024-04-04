8099ef7a3813e5493295b9539b52359faeea2a0a
mrugacz <mrugalski.kuba@gmail.com>
2024-04-05 00:32:19 +0200

    fixed battery icon

--- .config/ags/statusBar.js
+++ .config/ags/statusBar.js
@@ -9,46 +9,46 @@ const systemtray = await Service.import("systemtray");
 //          ╰─────────────────────────────────────────────────────────╯
 
 function Workspaces(monitor) {
-    const activeId = hyprland.active.workspace.bind("id");
-    const workspaces = hyprland.bind("workspaces").transform((allWorkspaces) => {
-        return allWorkspaces
-            .filter(
-                (workspace) =>
-                    hyprland.getWorkspace(workspace.id).monitorID === monitor,
-            ) // Filter workspaces based on monitor
-            .sort((a, b) => {
-                // Sort workspaces by their names
-                const nameA = a.name.toLowerCase();
-                const nameB = b.name.toLowerCase();
-                if (nameA < nameB) return -1;
-                if (nameA > nameB) return 1;
-                return 0;
-            })
-            .map((workspace) => {
-                const workspaceName = workspace.name % 5;
-                return Widget.Button({
-                    label: `${workspaceName}`,
-                    on_clicked: () =>
-                        hyprland.messageAsync(`dispatch workspace ${workspace.id}`),
-                    class_name: activeId.as(
-                        (i) => `${i === workspace.id ? "focused" : ""}`,
-                    ),
-                });
-            });
-    });
+  const activeId = hyprland.active.workspace.bind("id");
+  const workspaces = hyprland.bind("workspaces").transform((allWorkspaces) => {
+    return allWorkspaces
+      .filter(
+        (workspace) =>
+          hyprland.getWorkspace(workspace.id).monitorID === monitor,
+      ) // Filter workspaces based on monitor
+      .sort((a, b) => {
+        // Sort workspaces by their names
+        const nameA = a.name.toLowerCase();
+        const nameB = b.name.toLowerCase();
+        if (nameA < nameB) return -1;
+        if (nameA > nameB) return 1;
+        return 0;
+      })
+      .map((workspace) => {
+        const workspaceName = workspace.name % 5;
+        return Widget.Button({
+          label: `${workspaceName}`,
+          on_clicked: () =>
+            hyprland.messageAsync(`dispatch workspace ${workspace.id}`),
+          class_name: activeId.as(
+            (i) => `${i === workspace.id ? "focused" : ""}`,
+          ),
+        });
+      });
+  });
 
-    return Widget.Box({
-        class_name: "workspaces",
-        children: workspaces,
-    });
+  return Widget.Box({
+    class_name: "workspaces",
+    children: workspaces,
+  });
 }
 
 function ClientTitle() {
-    return Widget.Label({
-        class_name: "client-title",
-        label: hyprland.active.client.bind("title"),
-        truncate: "end",
-    });
+  return Widget.Label({
+    class_name: "client-title",
+    label: hyprland.active.client.bind("title"),
+    truncate: "end",
+  });
 }
 
 //          ╭─────────────────────────────────────────────────────────╮
@@ -56,14 +56,14 @@ function ClientTitle() {
 //          ╰─────────────────────────────────────────────────────────╯
 
 const date = Variable("", {
-    poll: [1000, 'date "+%H:%M - %e %B %Y"'],
+  poll: [1000, 'date "+%H:%M - %e %B %Y"'],
 });
 
 function Clock() {
-    return Widget.Label({
-        class_name: "clock",
-        label: date.bind(),
-    });
+  return Widget.Label({
+    class_name: "clock",
+    label: date.bind(),
+  });
 }
 
 //          ╭─────────────────────────────────────────────────────────╮
@@ -71,142 +71,134 @@ function Clock() {
 //          ╰─────────────────────────────────────────────────────────╯
 
 function Media() {
-    const label = Utils.watch("", mpris, "player-changed", () => {
-        if (mpris.players[0]) {
-            const { track_artists, track_title } = mpris.players[0];
-            return `${track_artists.join(", ")} - ${track_title}`;
-        } else {
-            return "Nothing is playing";
-        }
-    });
+  const label = Utils.watch("", mpris, "player-changed", () => {
+    if (mpris.players[0]) {
+      const { track_artists, track_title } = mpris.players[0];
+      return `${track_artists.join(", ")} - ${track_title}`;
+    } else {
+      return "Nothing is playing";
+    }
+  });
 
-    return Widget.Button({
-        class_name: "media",
-        on_primary_click: () => mpris.getPlayer("")?.playPause(),
-        on_scroll_up: () => mpris.getPlayer("")?.next(),
-        on_scroll_down: () => mpris.getPlayer("")?.previous(),
-        child: Widget.Label({ label: label, truncate: "end" }),
-    });
+  return Widget.Button({
+    class_name: "media",
+    on_primary_click: () => mpris.getPlayer("")?.playPause(),
+    on_scroll_up: () => mpris.getPlayer("")?.next(),
+    on_scroll_down: () => mpris.getPlayer("")?.previous(),
+    child: Widget.Label({ label: label, truncate: "end" }),
+  });
 }
 
 function Volume() {
-    const icons = {
-        101: "overamplified",
-        67: "high",
-        34: "medium",
-        1: "low",
-        0: "muted",
-    };
+  const icons = {
+    101: "overamplified",
+    67: "high",
+    34: "medium",
+    1: "low",
+    0: "muted",
+  };
 
-    function getIcon() {
-        const icon = audio.speaker.is_muted
-            ? 0
-            : [101, 67, 34, 1, 0].find(
-                (threshold) => threshold <= audio.speaker.volume * 100,
-            );
+  function getIcon() {
+    const icon = audio.speaker.is_muted
+      ? 0
+      : [101, 67, 34, 1, 0].find(
+          (threshold) => threshold <= audio.speaker.volume * 100,
+        );
 
-        return `audio-volume-${icons[icon]}-symbolic`;
-    }
+    return `audio-volume-${icons[icon]}-symbolic`;
+  }
 
-    const icon = Widget.Icon({
-        icon: Utils.watch(getIcon(), audio.speaker, getIcon),
-    });
+  const icon = Widget.Icon({
+    icon: Utils.watch(getIcon(), audio.speaker, getIcon),
+  });
 
-    const slider = Widget.Slider({
-        hexpand: true,
-        draw_value: false,
-        on_change: ({ value }) => (audio.speaker.volume = value),
-        setup: (self) =>
-            self.hook(audio.speaker, () => {
-                self.value = audio.speaker.volume || 0;
-            }),
-    });
+  const slider = Widget.Slider({
+    hexpand: true,
+    draw_value: false,
+    on_change: ({ value }) => (audio.speaker.volume = value),
+    setup: (self) =>
+      self.hook(audio.speaker, () => {
+        self.value = audio.speaker.volume || 0;
+      }),
+  });
 
-    return Widget.Box({
-        class_name: "volume",
-        css: "min-width: 180px",
-        children: [icon, slider],
-    });
+  return Widget.Box({
+    class_name: "volume",
+    css: "min-width: 180px",
+    children: [icon, slider],
+  });
 }
 
 const Microphone = () =>
-    Widget.Button({
-        class_name: "microphone",
-        on_primary_click: () =>
-            Utils.exec(
-                'bash -c "pactl set-source-mute `pactl get-default-source` toggle"',
-            ),
-        child: Widget.Icon().hook(audio, (self) => {
-            if (!audio.microphone || !audio.microphone.stream) return;
+  Widget.Button({
+    class_name: "microphone",
+    on_primary_click: () =>
+      Utils.exec(
+        'bash -c "pactl set-source-mute `pactl get-default-source` toggle"',
+      ),
+    child: Widget.Icon().hook(audio, (self) => {
+      if (!audio.microphone || !audio.microphone.stream) return;
 
-            const icon = audio.microphone.stream.is_muted
-                ? "microphone-sensitivity-muted-symbolic"
-                : "microphone-sensitivity-high-symbolic";
-            self.icon = icon;
-        }),
-    });
+      const icon = audio.microphone.stream.is_muted
+        ? "microphone-sensitivity-muted-symbolic"
+        : "microphone-sensitivity-high-symbolic";
+      self.icon = icon;
+    }),
+  });
 
 function formatTime(seconds) {
-    if (isNaN(seconds) || seconds < 0) {
-        return 'Invalid time';
-    }
+  if (isNaN(seconds) || seconds < 0) {
+    return "Invalid time";
+  }
 
-    const hours = Math.floor(seconds / 3600);
-    const minutes = Math.floor((seconds % 3600) / 60);
-    const remainingSeconds = Math.floor(seconds % 60);
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const remainingSeconds = Math.floor(seconds % 60);
 
-    const hoursDisplay = hours > 0 ? hours + 'h ' : '';
-    const minutesDisplay = minutes > 0 ? minutes + 'm ' : '';
-    const secondsDisplay = remainingSeconds > 0 ? remainingSeconds + 's' : '';
+  const hoursDisplay = hours > 0 ? hours + "h " : "";
+  const minutesDisplay = minutes > 0 ? minutes + "m " : "";
+  const secondsDisplay = remainingSeconds > 0 ? remainingSeconds + "s" : "";
 
-    return hoursDisplay + minutesDisplay + secondsDisplay;
+  return hoursDisplay + minutesDisplay + secondsDisplay;
 }
 
 function BatteryLabel() {
-    if (!battery.available) return;
+  if (!battery.available) return; // Ignore if no battery is present
+  const percent = Math.max(0, battery.percent / 100);
+  const icon = `battery-level-${Math.floor(percent * 10) * 10}-symbolic`;
+  const label = ` ${Math.floor(percent * 100)}%`;
+  const time = formatTime(battery.time_remaining);
 
-    const percent = Math.floor(Math.max(0, battery.percent));
-    const icon = `battery-level-${percent}-symbolic`;
-    const label = `${percent}%`;
-    const time = formatTime(battery.time_remaining);
-
-    return Widget.Box({
-        class_name: "battery",
-        visible: battery.available,
-        tooltip_text: time ? `Time remaining: ${time}` : "Charging",
-        children: [
-            Widget.Icon({ icon }),
-            Widget.Label({ label }),
-        ],
-    });
+  return Widget.Box({
+    class_name: "battery",
+    visible: battery.available,
+    tooltip_text: time ? `Time remaining: ${time}` : "Charging",
+    children: [Widget.Icon({ icon }), Widget.Label({ label })],
+  });
 }
-
-
-
 
 /* label: Battery.bind('percent').transform(p => {
                 return ` ${Math.floor(p)}%`; */
-
 
 //          ╭─────────────────────────────────────────────────────────╮
 //          │                       System Tray                       │
 //          ╰─────────────────────────────────────────────────────────╯
 
 function SysTray() {
-    const items = systemtray.bind("items").as((items) =>
-        items.map((item) =>
-            Widget.Button({
-                child: Widget.Icon({ icon: item.bind("icon") }),
-                on_primary_click: (_, event) => item.activate(event),
-                on_secondary_click: (_, event) => item.openMenu(event),
-                tooltip_markup: item.bind("tooltip_markup"),
-            }),
-        ),
-    );
+  const items = systemtray.bind("items").as((items) =>
+    items.map((item) =>
+      Widget.Button({
+        child: Widget.Icon({ icon: item.bind("icon") }),
+        on_primary_click: (_, event) => item.activate(event),
+        on_secondary_click: (_, event) => item.openMenu(event),
+        tooltip_markup: item.bind("tooltip_markup"),
+      }),
+    ),
+  );
 
-    return Widget.Box({
-        children: items,
-    });
+  return Widget.Box({
+    children: items,
+  });
 }
 
 //          ╭─────────────────────────────────────────────────────────╮
@@ -214,25 +206,25 @@ function SysTray() {
 //          ╰─────────────────────────────────────────────────────────╯
 
 function Left(monitor) {
-    return Widget.Box({
-        spacing: 8,
-        children: [Workspaces(monitor), ClientTitle()],
-    });
+  return Widget.Box({
+    spacing: 8,
+    children: [Workspaces(monitor), ClientTitle()],
+  });
 }
 
 function Center() {
-    return Widget.Box({
-        spacing: 8,
-        children: [Clock()],
-    });
+  return Widget.Box({
+    spacing: 8,
+    children: [Clock()],
+  });
 }
 
 function Right() {
-    return Widget.Box({
-        hpack: "end",
-        spacing: 8,
-        children: [Media(), Microphone(), Volume(), BatteryLabel(), SysTray()],
-    });
+  return Widget.Box({
+    hpack: "end",
+    spacing: 8,
+    children: [Media(), Microphone(), Volume(), BatteryLabel(), SysTray()],
+  });
 }
 
 //          ╭─────────────────────────────────────────────────────────╮
@@ -240,16 +232,16 @@ function Right() {
 //          ╰─────────────────────────────────────────────────────────╯
 
 export function StatusBar(monitor) {
-    return Widget.Window({
-        name: `bar-${monitor}`, // name has to be unique
-        class_name: "bar",
-        monitor,
-        anchor: ["top", "left", "right"],
-        exclusivity: "exclusive",
-        child: Widget.CenterBox({
-            start_widget: Left(monitor),
-            center_widget: Center(),
-            end_widget: Right(),
-        }),
-    });
+  return Widget.Window({
+    name: `bar-${monitor}`, // name has to be unique
+    class_name: "bar",
+    monitor,
+    anchor: ["top", "left", "right"],
+    exclusivity: "exclusive",
+    child: Widget.CenterBox({
+      start_widget: Left(monitor),
+      center_widget: Center(),
+      end_widget: Right(),
+    }),
+  });
 }
