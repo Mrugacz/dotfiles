137b0a4616d76c5eea0ff3d66d13f870fd3e8ebc
mrugacz <mrugalski.kuba@gmail.com>
2024-05-30 21:55:00 +0200

    updated ssh script

--- .config/scripts/rofi/rofi-ssh.sh
+++ .config/scripts/rofi/rofi-ssh.sh
@@ -2,13 +2,12 @@
 # should work with dmenu
 # fix gnome keyring
 export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/gcr/ssh"
-SERVER=$(echo -e "mrugacz.xyz\nc1872.lh.pl\nvoron.mrugacz.xyz\nvps.mrugacz.xyz" | rofi -dmenu)
+SERVER=$(echo -e "mrugacz.xyz\nc1872.lh.pl\nvoron.mrugacz.xyz\nvps.mrugacz.xyz\nhetzner-backup" | rofi -dmenu)
 
-TERMCMD='kitty -e ssh'
-
-case $SERVER in 
-	mrugacz.xyz) $TERMCMD mrugacz.xyz ;;
-	c1872.lh.pl) $TERMCMD c1872.lh.pl ;;
-	voron.mrugacz.xyz) $TERMCMD voron.mrugacz.xyz ;;
-	vps.mrugacz.xyz) $TERMCMD vps.mrugacz.xyz
+case $SERVER in
+mrugacz.xyz) kitty -e ssh mrugacz.xyz ;;
+c1872.lh.pl) kitty -e ssh c1872.lh.pl ;;
+voron.mrugacz.xyz) kitty -e ssh voron.mrugacz.xyz ;;
+vps.mrugacz.xyz) kitty -e ssh vps.mrugacz.xyz ;;
+hetzner-backup) kitty -e ssh hetzner-backup ;;
 esac
