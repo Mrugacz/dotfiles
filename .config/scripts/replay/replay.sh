18ca714dbf8d6ab0ba52352d29d7101f17f9de07
mrugacz <mrugalski.kuba@gmail.com>
2024-06-03 22:17:42 +0200

    specified video codec

--- .config/scripts/replay/replay.sh
+++ .config/scripts/replay/replay.sh
@@ -4,4 +4,4 @@ source "$HOME/.config/scripts/replay/.env"
 
 notify-send --icon='video-display' 'Instant replay' 'Replay service started.'
 
-gpu-screen-recorder -w screen -f $FRAMERATE -a "$(pactl get-default-sink).monitor|$(pactl get-default-source)" -o $FILE_PATH -c mp4 -r $RECORD_TIME
+gpu-screen-recorder -k h264 -w screen -f $FRAMERATE -a "$(pactl get-default-sink).monitor|$(pactl get-default-source)" -o $FILE_PATH -c mp4 -r $RECORD_TIME
