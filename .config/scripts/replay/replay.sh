1ae61c757600f97a7108565d200e323090b7bd0c
mrugacz <mrugalski.kuba@gmail.com>
2024-07-17 00:34:57 +0200

    updated replay scripts to automatically arrange files

--- .config/scripts/replay/replay.sh
+++ .config/scripts/replay/replay.sh
@@ -4,4 +4,5 @@ source "$HOME/.config/scripts/replay/.env"
 
 notify-send --icon='video-display' 'Instant replay' 'Replay service started.'
 
-gpu-screen-recorder -k h264 -w screen -f $FRAMERATE -a "$(pactl get-default-sink).monitor|$(pactl get-default-source)" -o $FILE_PATH -c mp4 -r $RECORD_TIME
+# gpu-screen-recorder -k h264 -w screen -f $FRAMERATE -a "$(pactl get-default-sink).monitor|$(pactl get-default-source)" -o $FILE_PATH -c mp4 -r $RECORD_TIME
+gpu-screen-recorder -k h264 -w screen -f $FRAMERATE -a "$(pactl get-default-sink).monitor|$(pactl get-default-source)" -sc "$HOME/.config/scripts/replay/rename.sh" -o $FILE_PATH -c mp4 -r $RECORD_TIME
--- .config/scripts/replay/save-replay.sh
+++ .config/scripts/replay/save-replay.sh
@@ -2,9 +2,9 @@
 
 source "$HOME/.config/scripts/replay/.env"
 
-if [[ $(ps aufx | grep gpu-screen-recorder | grep -vc grep ) -eq 0 ]]; then
+if [[ $(ps aufx | grep gpu-screen-recorder | grep -vc grep) -eq 0 ]]; then
 	notify-send --icon='record-desktop' 'Instant replay' "Replay service is not running."
 else
 	killall -SIGUSR1 gpu-screen-recorder
-	notify-send --icon='green-recorder' 'Instant replay' "Saving last $RECORD_TIME seconds."
+	#  notify-send --icon='green-recorder' 'Instant replay' "Saving last $RECORD_TIME seconds."
 fi
